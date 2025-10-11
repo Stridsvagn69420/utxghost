0e9accf23dc22dae6a336337b640b3553e8ada45
Stridsvagn69420 <Stridsvagn69420@proton.me>
2025-10-11 03:48:13 +0200

    totally didn't miss a newline...

--- main.c
+++ main.c
@@ -1,14 +1,17 @@
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <unistd.h>
 #include <utmpx.h>
 
 int main() {
-	// Hello World (exploring unistd.h)
 	char hostname[24];
 	gethostname(hostname, 24);
 	printf("Hello, %s@%s on %s!\n\n", getlogin(), hostname, ttyname(STDOUT_FILENO));
 
-	// Note to self: either I'm stupid or those predefined functions are kind of dumb because I'll have to "fread() the data into the struct" anyway
+	// This only works with glibc, so basically Linux only.
+	utmpxname(UTMPX_FILENAME);
+	setutxent();
 	struct utmpx *data;
 	data = getutxent();
 
@@ -17,14 +20,15 @@ int main() {
 		return 1;
 	}
 
-	// This also doesn't always output, or data is *very* delayed...
 	while (data != NULL) {
 		printf("%.*d ", (int)(sizeof data->ut_pid), data->ut_pid);
 		printf("%.*s ", (int)(sizeof data->ut_line), data->ut_line);
 		printf("%.*s ", (int)(sizeof data->ut_id), data->ut_id);
-		printf("%.*s ", (int)(sizeof data->ut_user), data->ut_user);
+		printf("%.*s\n", (int)(sizeof data->ut_user), data->ut_user);
 
 		data = getutxent();
 	}
+
+	endutxent();
 	return 0;
 }
